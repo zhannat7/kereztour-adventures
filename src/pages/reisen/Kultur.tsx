c8c23d4239255b0d1fe623c2c4c25b5344ea399a
Zhannat Kudaibergenova <zhannatkudaibergenova7@gmail.com>
2026-05-15 01:53:48 +0200

    Update Kultur.tsx

--- src/pages/reisen/Kultur.tsx
+++ src/pages/reisen/Kultur.tsx
@@ -186,7 +186,7 @@ const Kultur = () => {
           }
         });
       },
-      { threshold: 0.3, rootMargin: "-20% 0px -60% 0px" }
+      { threshold: 0.1, rootMargin: "-10% 0px -50% 0px" }
     );
     dayRefs.current.forEach((el) => { if (el) observer.observe(el); });
     return () => observer.disconnect();
