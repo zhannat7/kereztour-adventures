c231ee2292ec867ef4269e6b93a447b3340d9921
Zhannat Kudaibergenova <zhannatkudaibergenova7@gmail.com>
2026-05-15 18:24:16 +0200

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -105,7 +105,7 @@ const Lightbox = ({ images, index, onClose, onPrev, onNext }: {
   return (
     <div
       <div
-      className="fixed inset-0 z-[100] bg-black/50 backdrop-blur-md"
+  className="fixed inset-0 z-[100] bg-black/75"
       onClick={onClose}
     >
       {/* Counter */}
