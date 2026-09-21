7dd8f70d2987e6faab64e51d50078e8768076c6a
Zhannat Kudaibergenova <zhannatkudaibergenova7@gmail.com>
2026-09-21 18:47:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import Index from "./pages/Index.tsx";
 import Buchen from "./pages/Buchen.tsx";
 import Zahlung from "./pages/Zahlung.tsx";
+
 import Nomaden from "./pages/reisen/Nomaden.tsx";
 import Kultur from "./pages/reisen/Kultur.tsx";
 import Trekking from "./pages/reisen/Trekking.tsx";
+import Kyrchyn from "./pages/reisen/Kyrchyn.tsx";
+
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -18,18 +22,42 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+
           <Route path="/buchen" element={<Buchen />} />
           <Route path="/zahlung" element={<Zahlung />} />
-          <Route path="/reisen/nomaden" element={<Nomaden />} />
-          <Route path="/reisen/kultur" element={<Kultur />} />
-          <Route path="/reisen/trekking" element={<Trekking />} />
+
+          <Route
+            path="/reisen/nomaden"
+            element={<Nomaden />}
+          />
+
+          <Route
+            path="/reisen/kultur"
+            element={<Kultur />}
+          />
+
+          <Route
+            path="/reisen/trekking"
+            element={<Trekking />}
+          />
+
+          <Route
+            path="/reisen/kyrchyn"
+            element={<Kyrchyn />}
+          />
+
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route
+            path="*"
+            element={<NotFound />}
+          />
         </Routes>
       </BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
