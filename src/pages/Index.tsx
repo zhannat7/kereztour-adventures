b8dbf3eaefa73d68c93b1aa095d59a4c58e2789f
Zhannat Kudaibergenova <zhannatkudaibergenova7@gmail.com>
2026-09-22 16:08:44 +0200

    Show tour dates after hero

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import TourDates from "@/components/TourDates";
 import Highlights from "@/components/Highlights";
 import Tours from "@/components/Tours";
 import NomadGames from "@/components/NomadGames";
@@ -15,6 +16,7 @@ const Index = () => (
     <Navbar />
     <main>
       <Hero />
+      <TourDates />
       <Highlights />
       <Tours />
       <NomadGames />
