85fb82dd0aff4611db3f9e50168865651b723c67
Zhannat Kudaibergenova <zhannatkudaibergenova7@gmail.com>
2026-09-23 15:40:03 +0200

    Move tour dates into hero

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import TourDates from "@/components/TourDates";
 import Highlights from "@/components/Highlights";
 import Tours from "@/components/Tours";
 import NomadGames from "@/components/NomadGames";
@@ -17,7 +16,6 @@ const Index = () => (
     <Navbar />
     <main>
       <Hero />
-      <TourDates />
       <Highlights />
       <Tours />
       <NomadGames />
